377c3beb01ab6dc4e4e586e2640acc2b43fe9889
Elma Rahman <elmarahman635@gmail.com>
2019-05-21 23:59:04 -0400

    Done.

---
+++ lib/class_methods_module.rb
@@ -0,0 +1,8 @@
+# shareable class method, metadata - which will report on some pertinent (shared) information regarding both classes.
+
+module MetaDancing
+
+  def metadata
+    "This class produces objects that love to dance."
+  end
+end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,21 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
+
+class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+
+#To lend our two classes all of the methods of the Dance module, we use the include keyword:
+
+# If we use include keyword, we allow our classes to use all of the methods of the included module as instance methods.
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
